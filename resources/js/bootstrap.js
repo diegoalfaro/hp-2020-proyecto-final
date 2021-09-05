f862a7daa9a1fbb1d58b639ace6f9c8ca41e9cf0
dalfaro <dalfaro@navent.com>
2021-09-05 16:12:02 -0300

    Mejoras visuales y de funcionamiento

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,17 +1,31 @@
-try {
+const loadDependencies = () => {
   window._ = require('lodash');
   window.$ = window.jQuery = require('jquery');
   window.bootstrap = require('bootstrap');
   window.feather = require('feather-icons');
   window.axios = require('axios');
   window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+};
+
+const loadIcons = () => {
+  feather.replace();
+};
+
+const loadTooltips = () => {
+  $('[data-bs-toggle="tooltip"]').tooltip();
+};
+
+try {
+  loadDependencies();
 
   $(function(){
-    feather.replace();
-    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-    tooltipTriggerList.map(function (tooltipTriggerEl) {
-      return new bootstrap.Tooltip(tooltipTriggerEl)
-    })
+    loadIcons();
+    loadTooltips();
+
+    $(document).on('DOMNodeInserted', function(e) {
+      loadIcons();
+      loadTooltips();
+    });
   });
 } catch (e) {
   console.error('Error al cargar las dependencias')
