a15e241fff5527876f46a4fb2575546b08443342
clear <42496781+JuanIgnacioN@users.noreply.github.com>
2021-09-07 19:37:21 -0300

    Se agregan algunas migraciones

---
+++ database/migrations/2021_09_07_004314_create_vehicles_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVehiclesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vehicles', function (Blueprint $table) {
+            $table->id();
+            $table->varchar(domain);
+            $table->foreign('mark')->reference('id')->on('marks');
+            $table->string('model');
+            $table->integer('year');
+            $table->string('observations');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vehicles');
+    }
+}
