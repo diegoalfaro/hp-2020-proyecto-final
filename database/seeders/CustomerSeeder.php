9c6fd69f4eccd9206d5084d21964c594f9a9df09
dalfaro-navent <dalfaro@navent.com>
2021-09-07 19:42:43 -0300

    Agregar CustomerSeeder

--- database/seeders/CustomerSeeder.php
+++ database/seeders/CustomerSeeder.php
@@ -3,15 +3,17 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\Customer;
 
-class DatabaseSeeder extends Seeder
+class CustomerSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
+        Customer::factory(50)->create();
     }
 }
